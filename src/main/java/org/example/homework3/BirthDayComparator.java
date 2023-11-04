3c7942857f88d14342d16cd4f8706b13b281e18d
Serega S <serega1067@yandex.ru>
2023-11-04 14:36:36 +0700

    Task homework 3 done

---
+++ src/main/java/org/example/homework3/BirthDayComparator.java
@@ -0,0 +1,13 @@
+package org.example.homework3;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+
+public class BirthDayComparator  implements  Comparator<Person>{
+
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        return o1.getBirthday().compareTo(o2.getBirthday());
+    }
+}
